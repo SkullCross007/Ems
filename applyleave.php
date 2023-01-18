a45a6ad761308acd0bc0feae605604ee60c66a07
Somanyu <somanyu.03samal@gmail.com>
2023-01-18 22:26:31 +0530

    ✅ update: applyleave.php

--- applyleave.php
+++ applyleave.php
@@ -9,129 +9,99 @@
 ?>
 
 <html>
-<head>
-	<title>Apply Leave | Employee Panel | XYZ Corporation</title>
-	<link rel="stylesheet" type="text/css" href="styleapply.css">
-</head>
-<body bgcolor="#F0FFFF">
-	
-	<header>
-		<nav>
-			<h1>XYZ Corp.</h1>
-			<ul id="navli">
-				<li><a class="homeblack" href="eloginwel.php?id=<?php echo $id?>"">HOME</a></li>
-				<li><a class="homeblack" href="myprofile.php?id=<?php echo $id?>"">My Profile</a></li>
-				<li><a class="homeblack" href="empproject.php?id=<?php echo $id?>"">My Projects</a></li>
-				<li><a class="homered" href="applyleave.php?id=<?php echo $id?>"">Apply Leave</a></li>
-				<li><a class="homeblack" href="elogin.html">Log Out</a></li>
-			</ul>
-		</nav>
-	</header>
-	 
-	<div class="divider"></div>
-	<div class="page-wrapper bg-blue p-t-100 p-b-100 font-robo">
-        <div class="wrapper wrapper--w680">
-            <div class="card card-1">
-                <div class="card-heading"></div>
-                <div class="card-body">
-                    <h2 class="title">Apply Leave Form</h2>
-                    <form action="process/applyleaveprocess.php?id=<?php echo $id?>" method="POST">
-
-
-                        <div class="input-group">
-                            <input class="input--style-1" type="text" placeholder="Reason" name="reason">
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                            	<p>Start Date</p>
-                                <div class="input-group">
-                                    <input class="input--style-1" type="date" placeholder="start" name="start">
-                                   
-                                </div>
-                            </div>
-                            <div class="col-2">
-                            	<p>End Date</p>
-                                <div class="input-group">
-                                    <input class="input--style-1" type="date" placeholder="end" name="end">
-                                   
-                                </div>
-                            </div>
-                        </div>
-                        
-
-
-
-                        <div class="p-t-20">
-                            <button class="btn btn--radius btn--green" type="submit">Submit</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
+		<!-- Google Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+		
+		<!-- Bootstrap CSS Link -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		
+		<!-- Custom CSS Links -->
+		<!-- <link rel="stylesheet" href="random.css"> -->
+		<!-- <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet"> -->
+		<title>MANAGFI</title>
+	</head>
+<body>
+	
+		<!-- Navbar -->
+		<nav class="navbar navbar-light navbar-expand-md py-3">
+			<div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span style="font-weight: bold;">XYZ Corporation</span></a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+				<div id="navcol-2" class="collapse navbar-collapse" style="font-size: 17px;">
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item"><a class="nav-link active" href="eloginwel.php?id=<?php echo $id?>">Home</a></li>
+						<li class="nav-item"><a class="nav-link active" href="myprofile.php?id=<?php echo $id?>">My Profile</a></li>
+						<li class="nav-item"><a class="nav-link active" href="empproject.php?id=<?php echo $id?>">My Projects</a></li>
+						<li class="nav-item"><a class="nav-link active" href="applyleave.php?id=<?php echo $id?>">Apply Leave</a></li>
+					</ul><a class="btn btn-primary ms-md-2" role="button" href="alogin.html" style="padding-right: 18px;padding-left: 18px;font-size: 17px;">Logout</a>
+				</div>
+			</div>
+		</nav>
+	 
 
+	<h1 class="mt-4 mb-4 text-center">Apply Leave</h1>
 
+	<section class="position-relative py-4 py-xl-5">
+		<div class="container">
+			<div class="row d-flex justify-content-center">
+				<div class="col-md-6 col-xl-4">
+					<div class="card mb-5">
+						<div class="card-body d-flex flex-column align-items-center">
+							<form class="text-center" action="process/applyleaveprocess.php?id=<?php echo $id?>" method="POST">
+								<div class="d-lg-flex justify-content-center justify-content-lg-center align-items-lg-center mb-3">
+									<div class="text-start"><label class="form-label text-start" style="margin-bottom: 0px;">From</label><input class="form-control" name="start" placeholder="First Name" required style="margin-top: 6px;margin-bottom: 5px;margin-right: 7px;" type="date" /></div>
+									<div class="text-start"><label class="form-label text-start" style="margin-bottom: 0px;">To</label><input class="form-control" name="end" placeholder="First Name" required style="margin-top: 6px;margin-bottom: 5px;margin-right: 7px;" type="date" /></div>
+								</div>
+								<div class="mb-3"><input class="form-control" type="number" name="reason" placeholder="Reason" required /></div>
+								<div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Submit</button></div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<table>
-			<tr>
-				<th align = "center">Emp. ID</th>
-				<th align = "center">Name</th>
-				<th align = "center">Start Date</th>
-				<th align = "center">End Date</th>
-				<th align = "center">Total Days</th>
-				<th align = "center">Reason</th>
-				<th align = "center">Status</th>
-			</tr>
-
-
-			<?php
-
-
-				$sql = "Select employee.id, employee.firstName, employee.lastName, employee_leave.start, employee_leave.end, employee_leave.reason, employee_leave.status From employee, employee_leave Where employee.id = $id and employee_leave.id = $id order by employee_leave.token";
-				$result = mysqli_query($conn, $sql);
-				while ($employee = mysqli_fetch_assoc($result)) {
-					$date1 = new DateTime($employee['start']);
-					$date2 = new DateTime($employee['end']);
-					$interval = $date1->diff($date2);
-					$interval = $date1->diff($date2);
-
-					echo "<tr>";
-					echo "<td>".$employee['id']."</td>";
-					echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
-					
-					echo "<td>".$employee['start']."</td>";
-					echo "<td>".$employee['end']."</td>";
-					echo "<td>".$interval->days."</td>";
-					echo "<td>".$employee['reason']."</td>";
-					echo "<td>".$employee['status']."</td>";
-					
-				}
-
-
-			?>
-
-
-		</table>
-
-
-
-
-
-
-
+		<div class="container">
+			<div class="table-responsive">
+				<table class="table table-hover table-sm table-bordered">
+					<thead class="text-bg-primary">
+						<tr>
+							<th>Emp. ID</th>
+							<th>Name</th>
+							<th>Start Date</th>
+							<th>End Date</th>
+							<th>Total Days</th>
+							<th>Reason</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							$sql = "Select employee.id, employee.firstName, employee.lastName, employee_leave.start, employee_leave.end, employee_leave.reason, employee_leave.status From employee, employee_leave Where employee.id = $id and employee_leave.id = $id order by employee_leave.token";
+							$result = mysqli_query($conn, $sql);
+							while ($employee = mysqli_fetch_assoc($result)) {
+								$date1 = new DateTime($employee['start']);
+								$date2 = new DateTime($employee['end']);
+								$interval = $date1->diff($date2);
+								$interval = $date1->diff($date2);
+								echo "<tr>";
+								echo "<td>".$employee['id']."</td>";
+								echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
+								echo "<td>".$employee['start']."</td>";
+								echo "<td>".$employee['end']."</td>";
+								echo "<td>".$interval->days."</td>";
+								echo "<td>".$employee['reason']."</td>";
+								echo "<td>".$employee['status']."</td>";	
+							}
+						?>
+					</tbody>
+				</table>
+			</div>
+		</div>
 </body>
 </html>
